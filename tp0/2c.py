0c9a662851ed4d291cf4b8e048b455a6418da89e
marquitos.eth <mdedeu@itba.edu.ar>
2023-03-13 16:20:16 -0300

    style: clean code

--- tp0/2c.py
+++ tp0/2c.py
@@ -2,7 +2,6 @@ from src.pokemon import PokemonFactory, StatusEffect
 from src.functions import catch_with_pokeballs, get_avg_std_for_pokemon
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import json
 
 if __name__ == "__main__":
@@ -13,13 +12,13 @@ if __name__ == "__main__":
         pokemon_dictionary = dict(json.load(c))
     pokemon_names = pokemon_dictionary.keys()
 
+    # Crear subplots
+    fig1, ax1 = plt.subplots()
+
+    deltas = {}
     for name in pokemon_names:
         # Establecer el ancho de la barra
         width = 0.15
-        # Crear subplots
-        fig2, ax2 = plt.subplots()
-        fig3, ax3 = plt.subplots()
-        fig4, ax4 = plt.subplots()
 
         # Level
         level_attempts = []
@@ -28,21 +27,10 @@ if __name__ == "__main__":
         x = range(100)
         y = [level_attempts[i][0] for i in range(100)]
         yerr = [level_attempts[i][1] for i in range(100)]
-
         max_level = max(level_attempts[i][0] for i in range(100))
         min_level = min(level_attempts[i][0] for i in range(100))
-
         delta_level = max_level - min_level
         print("Delta Level: " + str(delta_level))
-
-        ax2.scatter(x, y)
-        ax2.errorbar(x, y, yerr=yerr, linestyle='None')
-        # Establecer etiquetas en los ejes y títulos
-        ax2.set_ylabel('Probabilidad de atrapar')
-        ax2.set_xlabel('Nivel')
-        ax2.set_title('Impacto del nivel: ' + name)
-        ax2.margins(x=0.02)
-        fig2.savefig('graphs/2c_' + name + '_level.png')
 
         # % HP
         hp_attempts = {}
@@ -51,21 +39,10 @@ if __name__ == "__main__":
         x = np.linspace(0, 1, 21)
         y = [hp_attempts[i][0] for i in np.linspace(0, 1, 21)]
         yerr = [hp_attempts[i][1] for i in np.linspace(0, 1, 21)]
-
         max_hp = max(hp_attempts[i][0] for i in np.linspace(0, 1, 21))
         min_hp = min(hp_attempts[i][0] for i in np.linspace(0, 1, 21))
-
         delta_hp = max_hp - min_hp
         print("Delta HP: " + str(delta_hp))
-
-
-        ax3.scatter(x, y)
-        ax3.errorbar(x, y, yerr=yerr, linestyle='None')
-        # Establecer etiquetas en los ejes y títulos
-        ax3.set_ylabel('Probabilidad de atrapar')
-        ax3.set_xlabel('HP %')
-        ax3.set_title('Impacto del HP %: ' + name)
-        fig3.savefig('graphs/2c_' + name + '_hp.png')
 
         # Status
         status_attempts = {}
@@ -74,20 +51,21 @@ if __name__ == "__main__":
         x = np.arange(len(StatusEffect))
         y = [status_attempts[i][0] for i in StatusEffect]
         yerr = [status_attempts[i][1] for i in StatusEffect]
-
         max_status = max(status_attempts[i][0] for i in StatusEffect)
         min_status = min(status_attempts[i][0] for i in StatusEffect)
-
         delta_status = max_status - min_status
         print("Delta Status: " + str(delta_status))
 
-        ax4.bar(x + width * 2, y, width, yerr=yerr)
-        # Establecer etiquetas en los ejes y títulos
-        ax4.set_ylabel('Probabilidad de atrapar')
-        ax4.set_xticks(np.arange(len(StatusEffect)) + width * 2)
-        ax4.set_xticklabels([status.name for status in StatusEffect])
-        ax4.set_title('Impacto del status: ' + name)
-        fig4.savefig('graphs/2c_' + name + '_status.png')
+        deltas[name] = (delta_hp, delta_level, delta_status)
+
+    # todo
+    # Graph with bars> Pokemon is the X axis.
+    # Each pokemon has three bars with different colors.
+    # Delta value is the Y Axis
+    #
+
+
+
 
 
 
