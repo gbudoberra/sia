bd29e80215830ddc3f995be3fdb00f33ccf47c00
gbudoberra <gbudoberra@itba.edu.ar>
2023-03-14 17:19:14 -0300

    fix: Changed graph style

--- tp0/2a.py
+++ tp0/2a.py
@@ -1,21 +1,35 @@
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
 
 if __name__ == "__main__":
     factory = PokemonFactory("pokemon.json")
     catch_prob = []
     condition_list = []
+    error = []
     for condition in StatusEffect:
-        average = []
+        data = []
         condition_list.append(str(condition.name))
         for _ in range(2000):
-            onix = factory.create("onix", 100, condition, 1)
-            value = 1 if attempt_catch(onix, "pokeball", 0.15)[0] else 0
-            average.append(value)
+            caterpie = factory.create("caterpie", 100, condition, 1)
+            value = 1.0 if attempt_catch(caterpie, "pokeball", 0.15)[0] else 0.0
+            data.append(value)
             # attempt_catch_by_pokeball.append(value)
-            # average.append(attempt_catch(onix, "heavyball", 0.15)[1])
-        catch_prob.append(sum(average) / len(average))
-    fig = px.scatter(x=condition_list, y=catch_prob, labels={'x': 'Health Condition', 'y': 'Catch Probability'})
+            # data.append(attempt_catch(caterpie, "heavyball", 0.15)[1])
+        catch_prob.append(sum(data) / len(data))
+        error.append(np.std(data)/np.sqrt(len(data)))
+        print(np.std(data))
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=condition_list, y=catch_prob, name='Catch Probability vs Health Condition',
+        error_y = dict(
+            type='data',
+            array=error,
+            visible=True
+        )
+    ))
     fig.update_layout(title='Catch Probability vs Health Condition')
     fig.show()
