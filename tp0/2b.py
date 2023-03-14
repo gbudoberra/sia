42709d9a562691e1bb38b5172f7abc9672dee08a
gbudoberra <gbudoberra@itba.edu.ar>
2023-03-14 17:18:48 -0300

    fix: Changed pokemon parameters

--- tp0/2b.py
+++ tp0/2b.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     for i in range(100):
         average = []
         for _ in range(2000):
-            onix = factory.create("onix", 100, StatusEffect.NONE, 0.01*i)
-            value = 1 if attempt_catch(onix, "pokeball", 0.15)[0] else 0
+            caterpie = factory.create("caterpie", 100, StatusEffect.NONE, 0.01*i)
+            value = 1 if attempt_catch(caterpie, "pokeball", 0.15)[0] else 0
             average.append(value)
             # attempt_catch_by_pokeball.append(value)
-            # average.append(attempt_catch(onix, "heavyball", 0.15)[1])
+            # average.append(attempt_catch(caterpie, "heavyball", 0.15)[1])
         catch_prob.append(sum(average) / len(average))
     fig = px.scatter(x=range(100), y=catch_prob, labels={'x': 'HP', 'y': 'Catch Probability'})
     fig.update_layout(title='Catch Probability vs HP')
