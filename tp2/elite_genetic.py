b76fd3592c912661be38ca150753e71c8dc668b2
gbudoberra <gbudoberra@itba.edu.ar>
2023-04-06 16:17:19 -0300

    fix: elite implements GenericSelectionMethod

--- tp2/elite_genetic.py
+++ tp2/elite_genetic.py
@@ -1,26 +1,18 @@
 from generic_genetic import GenericGenetic
+from tp2.generic_selection_method import GenericSelectionMethod
 
 
 def sort_by_fitness(genotype):
     return genotype.get_fitness()
 
 
-class EliteGenetic(GenericGenetic) :
+class EliteGenetic(GenericSelectionMethod):
 
-    def _select_parents(self):
-        pass
+    def __init__(self, size):
+        super().__init__(size)
 
-    def _crossover(self):
-        pass
-
-    def _mutate_population(self):
-        pass
-
-    def _generate_new_population(self):
+    def _select_parents(self, population):
         genotype_and_fitness = []
-        for genotype in self.population:
+        for genotype in population:
             genotype_and_fitness.append(genotype)
         return sorted(genotype_and_fitness, key=sort_by_fitness)[:self.new_generation_size]
-
-    def acceptable_solution(self):
-        pass
