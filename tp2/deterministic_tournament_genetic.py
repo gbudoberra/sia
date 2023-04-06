6de5a88998cf850e55068766eed3e3a8a98ed212
gbudoberra <gbudoberra@itba.edu.ar>
2023-04-06 16:17:37 -0300

    fix: deterministic Tournament implements GenericSelectionMethod

--- tp2/deterministic_tournament_genetic.py
+++ tp2/deterministic_tournament_genetic.py
@@ -1,22 +1,16 @@
 from generic_genetic import GenericGenetic
 from random import randint
 
+from tp2.generic_selection_method import GenericSelectionMethod
 
-class DeterministicTournamentGenetic(GenericGenetic):
 
-    def __init__(self, population, size, participant_number):
-        super().__init__(population,  size)
+class DeterministicTournamentGenetic(GenericSelectionMethod):
+
+    def __init__(self, size, participant_number):
+        super().__init__(size)
         self.participant_number = participant_number
-    def _select_parents(self):
-        pass
 
-    def _crossover(self):
-        pass
-
-    def _mutate_population(self):
-        pass
-
-    def _generate_new_population(self):
+    def _select_parents(self, population):
         current_new_generation_size = 0
         new_generation = []
         while current_new_generation_size < self.new_generation_size:
@@ -26,7 +20,7 @@ class DeterministicTournamentGenetic(GenericGenetic):
             while added_genotype < self.participant_number:
                 index = randint(0, self.new_generation_size - 1)
                 if index not in enrolled_genotypes:
-                    tournament_participant.append(self.population[index])
+                    tournament_participant.append(population[index])
                     added_genotype += 1
                     enrolled_genotypes.append(index)
             tournament_participant_fitness = [(genotype, genotype.get_fitness()) for genotype in tournament_participant]
@@ -34,6 +28,3 @@ class DeterministicTournamentGenetic(GenericGenetic):
             new_generation.append(tournament_participant_fitness[self.participant_number - 1])
             added_genotype += 1
         return new_generation
-
-    def acceptable_solution(self):
-        pass
