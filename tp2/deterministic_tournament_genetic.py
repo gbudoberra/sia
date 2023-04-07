2931a4198a7011bf47a778c8c3e0ee241f1ae5a3
marquitos.eth <mdedeu@itba.edu.ar>
2023-04-07 16:46:01 -0300

    fix: deterministic tournament

--- tp2/deterministic_tournament_genetic.py
+++ tp2/deterministic_tournament_genetic.py
@@ -1,3 +1,5 @@
+import random
+
 from generic_genetic import GenericGenetic
 from random import randint
 
@@ -6,6 +8,7 @@ from tp2.generic_selection_method import GenericSelectionMethod
 
 class DeterministicTournamentGenetic(GenericSelectionMethod):
 
+
     def __init__(self, size, participant_number):
         super().__init__(size)
         self.participant_number = participant_number
@@ -13,19 +16,13 @@ class DeterministicTournamentGenetic(GenericSelectionMethod):
     def _select_parents(self, population):
         current_new_generation_size = 0
         new_generation = []
+
         while current_new_generation_size < self.new_generation_size:
-            added_genotype = 0
-            enrolled_genotypes = []
-            tournament_participant = []
-            while added_genotype < self.participant_number:
-                index = randint(0, self.new_generation_size - 1)
-                if index not in enrolled_genotypes:
-                    tournament_participant.append(population[index])
-                    added_genotype += 1
-                    enrolled_genotypes.append(index)
-            tournament_participant_fitness = [(genotype, genotype.get_fitness()) for genotype in tournament_participant]
-            sorted(tournament_participant_fitness, key=lambda x: x[1])
-            new_generation.append(tournament_participant_fitness[self.participant_number - 1])
-            added_genotype += 1
+            sample_size = self.participant_number  # totally arbitrary number
+            tournament_participants = random.sample(population, sample_size)
+            tournament_participants_fitness = [(genotype, genotype.get_fitness()) for genotype in tournament_participants]
+            sorted(tournament_participants_fitness, key=lambda x: x[1])
+            new_generation.append(tournament_participants_fitness[self.participant_number - 1])
+            current_new_generation_size += 1
         return new_generation
 
--- tp2/main.py
+++ tp2/main.py
@@ -1,3 +1,4 @@
+from tp2.deterministic_tournament_genetic import DeterministicTournamentGenetic
 from tp2.elite_genetic import EliteGenetic
 from tp2.gentoype_impl import GenotypeImpl
 
@@ -8,7 +9,7 @@ if __name__ == '__main__':
         genotypes.append(GenotypeImpl())
         print(genotypes[i].value)
     print('--------')
-    method = EliteGenetic(10)
+    method = DeterministicTournamentGenetic(10, 4)
     parents = method._select_parents(genotypes)
     for p in parents:
-        print(p.get_fitness())
+        print(p[0].get_fitness())
