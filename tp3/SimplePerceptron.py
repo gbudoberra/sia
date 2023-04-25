eb21b695d8153ab08dda56fc80d860403fa134ad
shadad00 <shadad00@itba.edu.ar>
2023-04-25 17:41:17 -0300

    Simple perceptron finished.

--- tp3/SimplePerceptron.py
+++ tp3/SimplePerceptron.py
@@ -1,33 +1,48 @@
 import numpy as np
 
 
-class SingleLayerPerceptron:
+class SimplePerceptron:
+
+    activations_method = {
+        "step": [lambda x: np.sign(x)],
+        "id": [lambda x: x]
+    }
+
     def __init__(self, point_set, learning_rate, expected_results, epsilon, activation_function):
         if not point_set:
             raise ValueError("Invalid point provided")
         self.weights = np.zeros(len(point_set[0]) + 1)
-        self.point_matrix = np.array([np.insert(point, 0, 1) for point in point_set])
+        self.point_matrix = np.array([np.insert(point, 0, -1) for point in point_set])
         self.learning_rate = learning_rate
         self.expected_results = np.array(expected_results)
         self.current_weights_output = np.zeros(len(expected_results))
         self.current_iterations = 0
         self.epsilon = epsilon
-        self.activation_function = activation_function
+        self.activation_function = self.activations_method[activation_function]
 
     def has_converged(self):
-        output_results = [ self.activation_function(output) for output in self.current_weights_output]
+        output_results = [self.activation_function(output) for output in self.current_weights_output]
         return self.current_iterations > 1000 or \
             np.linalg.norm(output_results - self.expected_results) <= 0
 
     def update_weight(self, point, output_value, expected_value):
-        self.weights = self.weights + \
+        if output_value != expected_value:
+            self.weights = self.weights + \
                        (self.learning_rate * (expected_value - output_value) * point)
 
-    def get_solution(self):
+    def batch_iteration(self):
         while not self.has_converged():
             self.current_weights_output = np.dot(self.point_matrix, self.weights)
             for i in range(len(self.expected_results)):
                 vector_output = self.activation_function(self.current_weights_output[i])
-                if self.expected_results[i] != vector_output:
-                    self.update_weight(self.point_matrix[i], vector_output, self.expected_results[i])
+                self.update_weight(self.point_matrix[i], vector_output, self.expected_results[i])
             self.current_iterations += 1
+
+    def incremental_iteration(self):
+        while not self.has_converged():
+            for i in range(len(self.expected_results)):
+                self.current_weights_output[i] = np.dot(self.point_matrix[i], self.weights)
+                vector_output = self.activation_function(self.current_weights_output[i])
+                self.update_weight(self.point_matrix[i], vector_output, self.expected_results[i])
+            self.current_iterations += 1
+
--- tp3/main.py
+++ tp3/main.py
@@ -1,25 +1,22 @@
 import numpy as np
-from SingleLayerPerceptron  import SingleLayerPerceptron
+from SimplePerceptron  import SimplePerceptron
 import matplotlib.pyplot as plt
-
+from activation_function.activation_function import classifier
 
 def plot_result(w0, w1, w2, graph_points):
     x_vals = list(range(-2, 2))
-    y_vals = [(-w0 - w1 * x) / w2 for x in x_vals]
+    y_vals = [(w0 - w1 * x) / w2 for x in x_vals]
     plt.plot(x_vals, y_vals)
     for p in graph_points:
         plt.scatter(p[0], p[1], color='r')
     plt.show()
 
 
-def escalon(x):
-    return 1 if x >= 0 else -1
-
 if __name__ == '__main__':
     points = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
     expected = [-1, -1, -1, 1]
-    perceptron = SingleLayerPerceptron(points, 0.1, expected, 0.1, escalon)
-    perceptron.get_solution()
+    perceptron = SimplePerceptron(points, 0.1, expected, 0.1, classifier)
+    perceptron.incremental_iteration()
     result_weights = perceptron.weights
     print("Iterations: " + str(perceptron.current_iterations))
     plot_result(result_weights[0], result_weights[1], result_weights[2], points)
