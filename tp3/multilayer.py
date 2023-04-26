77e279a2d506d10ba8f63176dc09e63d0b772160
shadad00 <shadad00@itba.edu.ar>
2023-04-26 08:56:22 -0300

    feat: output calculation

--- tp3/multilayer.py
+++ tp3/multilayer.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+# based
+# on https://openlearninglibrary.mit.edu/assets/courseware/v1/9c36c444e5df10eef7ce4d052e4a2ed1/asset-v1:MITx+6.036+1T2019+type@asset+block/notes_chapter_Neural_Networks.pdf
 class Multilayer:
 
     activation_methods = {
@@ -8,21 +10,20 @@ class Multilayer:
         "id": [lambda x: x]
     }
 
-    # perceptron_by_layer provides the number of Perceptron in each layer from bottom
     def __init__(self, perceptron_by_layer, point_set, activation_method, expected_set, epsilon):
         self.layer_number = len(perceptron_by_layer)
         self.expected_matrix = np.array([point for point in expected_set])
-        point_matrix = np.array([np.insert(point, 0, -1) for point in point_set])
-        point_matrix = np.transpose(point_matrix)
-        self.output_by_layer = [point_matrix]
-        self.layer_weight_matrix_array = []
-        self.without_activation_by_layer = []
-        for index in range(len(perceptron_by_layer) - 1):
-            self.layer_weight_matrix_array.append(
+        input_matrix = np.array([np.insert(point, 0, -1) for point in point_set])
+        input_matrix = np.transpose(input_matrix)
+        self.output_by_layer = [input_matrix]
+        self.weights_by_layer = []
+        self.pre_activation_by_layer = []
+        for index in range(self.layer_number - 1):
+            self.weights_by_layer.append(
                 (np.zeros(perceptron_by_layer[index + 1], perceptron_by_layer[index])))
             self.output_by_layer.append(np.zeros((
                 perceptron_by_layer[index + 1], len(point_set))))
-            self.without_activation_by_layer.append(np.zeros(
+            self.pre_activation_by_layer.append(np.zeros(
                 (perceptron_by_layer[index + 1], len(point_set))))
         self.activation_method = np.vectorize(self.activation_methods[activation_method])
         self.epsilon = epsilon
@@ -38,9 +39,8 @@ class Multilayer:
         return self.error() < self.epsilon
 
     def batch_iteration(self):
-        for index in range(len(self.layer_weight_matrix_array)):
-            result = np.dot(self.layer_weight_matrix_array[index], self.output_by_layer[index - 1])
-            self.without_activation_by_layer[index] = result
-            self.output_by_layer[index] = self.activation_method(result)
+        for index in range(len(self.weights_by_layer)):
+            self.pre_activation_by_layer[index] = np.dot(self.weights_by_layer[index], self.output_by_layer[index - 1])
+            self.output_by_layer[index] = self.activation_method(self.pre_activation_by_layer[index])
 
 
