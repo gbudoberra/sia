b495f20eaff7463e51519cb9d6e6d646f1175d30
shadad00 <shadad00@itba.edu.ar>
2023-04-26 13:00:31 -0300

    starting back propagation method

--- tp3/main.py
+++ tp3/main.py
@@ -1,8 +1,7 @@
 import numpy as np
 from SimplePerceptron  import SimplePerceptron
 import matplotlib.pyplot as plt
-from activation_function.activation_function import classifier
-
+from multilayer import Multilayer
 def plot_result(w0, w1, w2, graph_points):
     x_vals = list(range(-2, 2))
     y_vals = [(w0 - w1 * x) / w2 for x in x_vals]
@@ -14,11 +13,12 @@ def plot_result(w0, w1, w2, graph_points):
 
 if __name__ == '__main__':
     points = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
-    expected = [-1, -1, -1, 1]
-    perceptron = SimplePerceptron(points, 0.1, expected, 0.1, classifier)
-    perceptron.incremental_iteration()
-    result_weights = perceptron.weights
-    print("Iterations: " + str(perceptron.current_iterations))
-    plot_result(result_weights[0], result_weights[1], result_weights[2], points)
+    expected = [[-1, 0], [-1, 1], [-1, 1], [1, 1]]
+    perceptron = Multilayer([3, 2, 2, 2], points, "id", expected, 0.1, 0.1)
+    perceptron.batch_iteration()
+    # perceptron.incremental_iteration()
+    # result_weights = perceptron.weights
+    # print("Iterations: " + str(perceptron.current_iterations))
+    # plot_result(result_weights[0], result_weights[1], result_weights[2], points)
 
 
--- tp3/multilayer.py
+++ tp3/multilayer.py
@@ -6,41 +6,66 @@ import numpy as np
 class Multilayer:
 
     activation_methods = {
-        "step": [lambda x: np.sign(x)],
-        "id": [lambda x: x]
+        "relu": [lambda x: np.maximum(0, x), lambda x: 1 if x > 0 else 0],
+        "sigmoid": [lambda x: 1 / (1 + np.exp(-x)), lambda x: (1 / (1 + np.exp(-x))) * (1 - (1 / (1 + np.exp(-x))))],
+        "id": [lambda x: x, lambda x: 1],
+        "tanh": [lambda x: np.tanh(x), lambda x: 1 - np.square(np.tanh(x))],
+        "step": [lambda x: np.sign(x), lambda x: 0],
     }
 
-    def __init__(self, perceptron_by_layer, point_set, activation_method, expected_set, epsilon):
+    def __init__(self, perceptron_by_layer, data_set, activation_method, result_set, epsilon, learning_rate):
+
+        self.point_number = len(data_set)
         self.layer_number = len(perceptron_by_layer)
-        self.expected_matrix = np.array([point for point in expected_set])
-        input_matrix = np.array([np.insert(point, 0, -1) for point in point_set])
-        input_matrix = np.transpose(input_matrix)
-        self.output_by_layer = [input_matrix]
+
+        self.results_matrix = np.transpose(np.array([point for point in result_set]))
+        self.input_matrix = np.transpose(np.array([np.insert(point, 0, -1) for point in data_set]))
+        self.activation_method = np.vectorize(self.activation_methods[activation_method][0])
+        self.activation_derivative = np.vectorize(self.activation_methods[activation_method][1])
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
+
+        self.output_by_layer = []
+        self.derivative_action_by_layer = []
         self.weights_by_layer = []
         self.pre_activation_by_layer = []
+
         for index in range(self.layer_number - 1):
-            self.weights_by_layer.append(
-                (np.zeros(perceptron_by_layer[index + 1], perceptron_by_layer[index])))
-            self.output_by_layer.append(np.zeros((
-                perceptron_by_layer[index + 1], len(point_set))))
-            self.pre_activation_by_layer.append(np.zeros(
-                (perceptron_by_layer[index + 1], len(point_set))))
-        self.activation_method = np.vectorize(self.activation_methods[activation_method])
-        self.epsilon = epsilon
+            self.weights_by_layer.append(np.zeros(
+                (perceptron_by_layer[index + 1], perceptron_by_layer[index])))
+            zero_matrix = np.zeros((perceptron_by_layer[index], self.point_number))
+            self.output_by_layer.append(zero_matrix.copy())
+            self.derivative_action_by_layer.append(zero_matrix.copy())
+            self.pre_activation_by_layer.append(zero_matrix.copy())
 
     def error(self):
-        transposed_result = np.transpose(self.output_by_layer[self.layer_number])
-        cumulative_error = 0
-        for index in range(len(transposed_result)):
-            cumulative_error += pow(transposed_result[index] - self.expected_matrix[index], 2)
-        return (1/2)*cumulative_error
+        error = (self.output_by_layer[self.layer_number - 1] - self.results_matrix) ** 2
+        cumulative_error = np.sum(error)
+        return (1 / 2) * cumulative_error
 
     def has_converged(self):
         return self.error() < self.epsilon
 
     def batch_iteration(self):
-        for index in range(len(self.weights_by_layer)):
-            self.pre_activation_by_layer[index] = np.dot(self.weights_by_layer[index], self.output_by_layer[index - 1])
-            self.output_by_layer[index] = self.activation_method(self.pre_activation_by_layer[index])
+        while not self.has_converged():
+            base = self.input_matrix
+            for index in range(self.layer_number - 1):
+                self.pre_activation_by_layer[index] = np.dot(self.weights_by_layer[index], base)
+                self.output_by_layer[index] = self.activation_method(self.pre_activation_by_layer[index])
+                self.derivative_action_by_layer[index] = self.activation_derivative(self.pre_activation_by_layer[index])
+                base = self.output_by_layer[index]
+            self.update_weights()
 
+    def update_weights(self):
+        for current_point in range(self.point_number):
+            current_layer = self.layer_number - 1
+            multiplier = self.learning_rate * (self.results_matrix.T[current_point] - self.output_by_layer[current_layer - 1].T[current_point])
+            layer_weight_delta = multiplier * self.output_by_layer[current_layer - 1].T[current_point]
+            layer_weight_delta *= self.derivative_action_by_layer[current_layer - 1].T[current_point]
+            self.weights_by_layer[current_layer - 1] += layer_weight_delta
+            while current_layer > 1:
+                layer_weight_delta = layer_weight_delta * self.weights_by_layer[current_layer - 1] * self.derivative_action_by_layer[current_layer - 1].T[current_point]
+                current_layer -= 1
+                self.weights_by_layer[current_layer] = layer_weight_delta
+        print("finished")
 
