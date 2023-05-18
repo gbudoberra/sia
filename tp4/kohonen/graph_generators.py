399307e11cffc4728a8b7b64e24fd7c15907660e
bsquillari <bsquillari@itba.edu.ar>
2023-05-18 12:21:25 -0300

    add: Plot heat map with names

--- tp4/kohonen/graph_generators.py
+++ tp4/kohonen/graph_generators.py
@@ -1,24 +1,41 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_heat_map(data):
-    max_row = max(data.keys(), key=lambda x: x[0])[0]
-    max_col = max(data.keys(), key=lambda x: x[1])[1]
-    country_matrix = np.zeros((max_row + 1, max_col + 1), dtype=int)
+def plot_heat_map(data, k):
+    country_matrix = np.zeros((k, k), dtype=int)
     for key, countries in data.items():
         row, col = key
         country_matrix[row, col] = len(countries)
 
     plt.imshow(country_matrix, cmap='YlOrRd')
-    plt.xticks(range(max_col + 1), range(max_col + 1))
-    plt.yticks(range(max_row + 1), range(max_row + 1))
+    plt.xticks(range(k), range(k))
+    plt.yticks(range(k), range(k))
 
     plt.xlabel('Columna')
     plt.ylabel('Fila')
-    for i in range(max_row + 1):
-        for j in range(max_col + 1):
+    for i in range(k):
+        for j in range(k):
             plt.text(j, i, country_matrix[i, j], ha='center', va='center', color='black', fontsize=8)
+    plt.colorbar()
+    plt.show()
+    plt.clf()
+
+
+def plot_heat_map_with_names(data, k):
+    country_matrix = np.zeros((k, k), dtype=int)
+    plt.figure(figsize=(20, 12))
+    for key, countries in data.items():
+        row, col = key
+        plt.text(col, row, '\n'.join(countries), ha='center', va='center', color='black', fontsize=12)
+        country_matrix[row, col] = len(countries)
+
+    plt.imshow(country_matrix, cmap='YlOrRd')
+    plt.xticks(range(k), range(k))
+    plt.yticks(range(k), range(k))
+
+    plt.xlabel('Columna')
+    plt.ylabel('Fila')
     plt.colorbar()
     plt.show()
 
--- tp4/kohonen/main.py
+++ tp4/kohonen/main.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
+
+from graph_generators import plot_assigned_categories, plot_heat_map, plot_avg_distance, plot_heat_map_with_names
 from tp4.kohonen.Som import Som
-from graph_generators import plot_heat_map, plot_avg_distance, plot_assigned_categories
 
 
 def parse_csv(filename='europe.csv'):
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     country_map = parse_csv()
     points = [points for points in country_map.values()]
     k = 8
-    som = Som(k, points, 500, True, True)
+    som = Som(k, points, 500, False, False)
     som.train()
 
     result_map = {}
@@ -32,9 +32,9 @@ if __name__ == '__main__':
         result_map.setdefault((row, col), []).append(country_name)
         category_map[country_name] = (row, col)
 
-    plot_heat_map(result_map)
+    plot_heat_map(result_map, k)
+    plot_heat_map_with_names(result_map, k)
     avg_distance = som.get_avg_distance()
     print(avg_distance)
     plot_avg_distance(avg_distance, k)
     # plot_assigned_categories(category_map)
-
